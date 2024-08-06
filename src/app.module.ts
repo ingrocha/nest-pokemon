de3c201da303bf5142b221071dde67d115d6fe4d
Carlos Rocha <ing_rocha@hotmail.com>
2024-08-06 16:28:14 -0700

    Preparar mongodb y envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
-import { EnvConfiguration } from './common/config/eNv.conf';
+import { EnvConfiguration } from './common/config/env.conf';
 import { joiValidationSchema } from './common/config/joi.validation';
 
 @Module({
